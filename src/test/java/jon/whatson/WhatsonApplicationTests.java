5dfde47ad39ef29844f03bc889a56a2cd34dd3e6
joneikholmkea <jone@kea.dk>
2022-10-05 15:48:45 +0200

    removed @Test

--- src/test/java/jon/whatson/WhatsonApplicationTests.java
+++ src/test/java/jon/whatson/WhatsonApplicationTests.java
@@ -3,10 +3,10 @@ package jon.whatson;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WhatsonApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
