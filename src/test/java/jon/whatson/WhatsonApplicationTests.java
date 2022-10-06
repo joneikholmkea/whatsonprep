0945be56f9a1770e48d895de238a179820054611
joneikholmkea <jone@kea.dk>
2022-10-06 15:50:43 +0200

    @DataJpaTest instead of @SpringBootTest

--- src/test/java/jon/whatson/WhatsonApplicationTests.java
+++ src/test/java/jon/whatson/WhatsonApplicationTests.java
@@ -1,12 +1,14 @@
 package jon.whatson;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
+@DataJpaTest
 class WhatsonApplicationTests {
 
-//    @Test was here
+    @Test //was here
     void contextLoads() {
     }
 
